6bc1ed3105985c2478a63d8af9dc439dfabf5d58
Victor Paranhos <victor_paranhos@hotmail.com>
2023-10-02 17:08:44 -0300

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,203 +1,119 @@
 import { Container, Content } from "./styles";
 import { Header } from "../../components/Header";
 import { Banner } from "../../components/Banner";
-import { Article } from "../../components/Article"
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, EffectFade } from 'swiper/modules';
-import { FiHeart } from "react-icons/fi"
+import { Article } from "../../components/Article";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, EffectFade } from "swiper/modules";
+import { FiHeart } from "react-icons/fi";
 import { Footer } from "../../components/Footer";
 import { Button } from "../../components/Button";
 import { useAuth } from "../../hooks/auth";
-import 'swiper/css';
-import 'swiper/css/bundle';
-import 'swiper/css/effect-fade';
-
+import "swiper/css";
+import "swiper/css/bundle";
+import "swiper/css/effect-fade";
+import { useState, useEffect } from "react";
 import { api } from "../../services/api";
 
 export function Home() {
-    const {user} = useAuth()
- 
-    async function handleDish(){
-        try{
-            await api.get("/dish")
+  const { user } = useAuth();
+  const [data, setData] = useState([]);
 
-
-        }catch(error){
-
-        }
-
-        
+  useEffect(() => {
+    async function handleDish() {
+      try {
+        const response = await api.get(`/dish/?title=${""}&ingredients=${[]}`);
+        setData(response.data);
+      } catch (error) {}
     }
 
-    return (
-        <Container>
-            <Header />
-            <Banner />
-            <Content>
+    handleDish();
+  }, []);
 
-            <Article title="Refeições">
-                <Swiper className="card-principal" modules={[Navigation, EffectFade]} navigation slidesPerView={'auto'} spaceBetween={27}>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-1.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-2.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Massa fresca com camarões e pesto. </p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-3.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-4.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-5.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-6.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-7.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                </Swiper>
-            </Article>
-
-
-            <Article title="Sobremesas">
-                <Swiper className="card-principal" modules={[Navigation, EffectFade]} navigation slidesPerView={'auto'} spaceBetween={27}>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-8.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-9.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-10.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-11.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-1.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-2.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                </Swiper>
-            </Article>
-            <Article title="Bebidas">
-                <Swiper className="card-principal" modules={[Navigation, EffectFade]} navigation slidesPerView={'auto'} spaceBetween={27}>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-3.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-4.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-8.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-9.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-10.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-5.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                    <SwiperSlide className="card">
-                        <div className="dish"><img src="/src/assets/Mask group-1.png" alt="" /><FiHeart size={24} /></div>
-                        <h1>Spaguetti Gambe</h1>
-                        <p>Presunto de parma e rúcula em um pão com fermentação natural.</p>
-                        <span>R$79,90</span>
-                        <Button />
-                    </SwiperSlide>
-                </Swiper>
-            </Article>
-            </Content>
-            <Footer />
-        </Container>
-    )
+  return (
+    <Container>
+      <Header />
+      <Banner />
+      <Content>
+        {data.some((item) => item.category === "Refeições") && (
+          <Article title="Refeições">
+            <Swiper
+              className="card-principal"
+              modules={[Navigation, EffectFade]}
+              navigation
+              slidesPerView={"auto"}
+              spaceBetween={27}
+            >
+              {data
+                .filter((item) => item.category === "Refeições")
+                .map((item, index) => (
+                  <SwiperSlide className="card">
+                    <div className="dish">
+                      <img src="/src/assets/Mask group-1.png" alt="" />
+                      <FiHeart size={24} />
+                    </div>
+                    <h1>{item.name}</h1>
+                    <p>{item.description}</p>
+                    <span>{item.price}</span>
+                    <Button />
+                  </SwiperSlide>
+                ))}
+            </Swiper>
+          </Article>
+        )}
+        {data.some((item) => item.category === "Sobremesas") && (
+          <Article title="Sobremesas">
+            <Swiper
+              className="card-principal"
+              modules={[Navigation, EffectFade]}
+              navigation
+              slidesPerView={"auto"}
+              spaceBetween={27}
+            >
+              {data
+                .filter((item) => item.category === "Sobremesas")
+                .map((item, index) => (
+                  <SwiperSlide className="card">
+                    <div className="dish">
+                      <img src="/src/assets/Mask group-1.png" alt="" />
+                      <FiHeart size={24} />
+                    </div>
+                    <h1>{item.name}</h1>
+                    <p>{item.description}</p>
+                    <span>{item.price}</span>
+                    <Button />
+                  </SwiperSlide>
+                ))}
+            </Swiper>
+          </Article>
+        )}
+        {data.some((item) => item.category === "Bebidas") && (
+          <Article title="Bebidas">
+            <Swiper
+              className="card-principal"
+              modules={[Navigation, EffectFade]}
+              navigation
+              slidesPerView={"auto"}
+              spaceBetween={27}
+            >
+              {data
+                .filter((item) => item.category === "Bebidas")
+                .map((item, index) => (
+                  <SwiperSlide className="card">
+                    <div className="dish">
+                      <img src="/src/assets/Mask group-1.png" alt="" />
+                      <FiHeart size={24} />
+                    </div>
+                    <h1>{item.name}</h1>
+                    <p>{item.description}</p>
+                    <span>{item.price}</span>
+                    <Button />
+                  </SwiperSlide>
+                ))}
+            </Swiper>
+          </Article>
+        )}
+      </Content>
+      <Footer />
+    </Container>
+  );
 }
